324dd7c1be5a7b6d8d7ab3d64eb0acb20a83246e
Juan Yu <sigrace0417@gmail.com>
2026-04-23 13:28:04 +0300

    L6

--- src/main/java/viikko4/lab/Book.java
+++ src/main/java/viikko4/lab/Book.java
@@ -1,5 +1,25 @@
 package viikko4.lab;
 
 public class Book {
-    
+    private String name;
+    private String author;
+    private int year;
+    public Book(String name, String author,int year){
+        if(name==null||name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        if(author==null||author.trim().isEmpty()){
+            throw new IllegalArgumentException("Author cannot be empty.");
+        }
+        if(year<0){
+            throw new IllegalArgumentException("Year cannot be negative.");
+        }
+        this.name=name;
+        this.author=author;
+        this.year=year;
+    }
+    public String getInfo(){
+        return author+": "+name+" ("+year+")";
+    }
+
 }
