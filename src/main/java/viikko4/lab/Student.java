aedb5a3b0fbe47b3b4675cac9a5d2009ef06fa5c
Juan Yu <sigrace0417@gmail.com>
2026-04-23 15:17:09 +0300

    L7

--- src/main/java/viikko4/lab/Student.java
+++ src/main/java/viikko4/lab/Student.java
@@ -1,5 +1,22 @@
 package viikko4.lab;
 
 public class Student {
-    
+    private String name;
+    private Integer points;
+    public Student(String name,Integer points){
+        if(name==null||name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        if(points==null||points<0){
+            throw new IllegalArgumentException("Points cannot be empty or negative.");
+        }
+        this.name=name;
+        this.points=points;
+    }
+    public String getName(){
+        return name;
+    }
+    public Integer getPoints(){
+        return points;
+    } 
 }
--- src/main/java/viikko4/lab/StudentRegister.java
+++ src/main/java/viikko4/lab/StudentRegister.java
@@ -1,5 +1,15 @@
 package viikko4.lab;
+import java.util.LinkedList;
 
 public class StudentRegister {
-    
+    private LinkedList<Student> students;
+
+    public StudentRegister(){
+        students=new LinkedList<>();
+    }
+    public void addStudent(String name,Integer points){
+        Student student=new Student(name,points);
+        students.add(student);
+    }
+
 }
